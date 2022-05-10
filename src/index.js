38cff526a07d9d7a62af2f141ab6006c2c2f0159
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-05-10 20:07:17 +0200

    Add html and js functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles/main.scss';
+import scoreArray from './modules/element.js';
+
+const displayBody = document.querySelector('.display_body');
+
+displayBody.innerHTML = '';
+scoreArray.forEach((score) => {
+  displayBody.innerHTML += `<li>Name: ${score}</li>`
+});
